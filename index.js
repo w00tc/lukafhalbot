eb6bc26e04ea82a0fae029ec09ec95bba02aa2ee
Luka Fhal <luka.fhal@trustalchemy.com>
2022-02-11 02:33:18 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const TelegramApi = require('node-telegram-bot-api');
+
+const token = "5253031559:AAGte7Ox31ahEOV9JDfxW-2mA06B03HSvgU";
+
+const bot = new TelegramApi(token, { polling: true });
+
+bot.on('message', msg => {
+    if(msg.text === "@luniela") {
+        bot.sendMessage(msg.from.id, "Да, вы абсолютно правы", {reply_to_message_id: msg.message_id});
+    }
+});
