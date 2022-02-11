7b269d4b912596371a4517e87fa92af44fc3fe40
Luka Fhal <luka.fhal@trustalchemy.com>
2022-02-11 11:09:01 +0100

    Port server

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const TelegramApi = require('node-telegram-bot-api');
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 const token = "5253031559:AAGte7Ox31ahEOV9JDfxW-2mA06B03HSvgU";
 
